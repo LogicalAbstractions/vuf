3e8db937d1bfed6f9673157b9a74e45ab16b813b
Bernhard Glück <business@bernhardglueck.com>
2022-10-27 14:03:25 +0200

    A3

--- train.py
+++ train.py
@@ -20,13 +20,13 @@ def top_2_accuracy(inp, targ, axis=-1):
 def execute_training():
 
     #dataset_path = "/media/bglueck/Data/Datasets/soccernet-datasets/4sec/large/train"
-    dataset_path = "/mnt/vol_b/data/4sec-5fps/large/train"
+    dataset_path = "/mnt/vol_b/data/4sec-25fps/large/train"
 
     configuration = {
-        "encoder": "resnet152",
+        "encoder": "resnet18",
         "model": "rnn",
         "dataset_path": dataset_path,
-        "batch_size": 2,
+        "batch_size": 8,
         "frame_size": 384,
         "val_split": 0.5,
         "frozen_epochs": 100,
